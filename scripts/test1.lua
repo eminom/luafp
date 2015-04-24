91dea868901fd86480962cb1b9ef27f192a8b4fb
eminemm <hai.bai.cn@gmail.com>
2015-04-24 17:01:47 +0800

    fixed

--- scripts/test1.lua
+++ scripts/test1.lua
@@ -11,11 +11,34 @@ end, function(err)
 	print(err)
 end)
 
+print("\n Testing + ")
 local b = func.CreateFix(1, 100)
 for i=1, 100 do
 	local c = func.CreateFix(i, 100)
 	b = b + c
 end
-
 print(b)
+print()
+
+print("\n Testing - ")
+for i=1, 100 do
+	local c = func.CreateFix(i, 100)
+	b = b - c 
+end
+print(b)
+print()
+
+local F = func.CreateFix
+local F0 = func.CreateFix0
+print("\n Test * ")
+do
+	local c = F(1, 2) * F(2, 3)
+	print(c)
+end
+
+print("\n Test / ")
+do
+	print(F0(1.2) / F0(0.6))
+end
+
 print("Test is done")
